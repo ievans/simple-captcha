563aa4cea90a63339c1a32e364b0d86130ed4f66
Christopher G Dolan <cdolan@mit.edu>
2011-10-29 21:26:34 -0400

    Skeleton bot

---
+++ bot.py
@@ -0,0 +1,13 @@
+import captcha
+import BeautifulSoup
+
+class bot:
+    def __init__(self, url):
+        self.zipCodes = {}
+        self.url = url
+
+    def submitVote(self):
+        
+        
+if __name__ == "__main__":
+    pass
